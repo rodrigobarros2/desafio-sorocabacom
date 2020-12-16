ad4d620824b6de00372b4ad207eda4010415b68f
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-12-16 14:35:08 -0300

    Add Back button

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -2,23 +2,25 @@ import styled from 'styled-components';
 import { device } from '../../config/device';
 
 export const Container = styled.div`
-	background-color: #7dede2;
-	display: flex;
-	justify-content: center;
 	align-items: center;
+	display: flex;
+	background-color: #7dede2;
 	flex-direction: column;
+	justify-content: center;
 `;
 
 export const Wrapper = styled.div`
+	display: flex;
 	background-color: white;
 	box-shadow: 0px 0px 6px #0000004d;
 	border-radius: 8px;
-	display: flex;
 	flex-direction: column;
-	width: 100%;
-	margin: -3% 0 -3% 0;
 	position: relative;
+	margin: -3% 0 -3% 0;
 	max-width: 1082px;
+	width: 100%;
+	position: relative;
+	z-index: 2;
 	form {
 		width: 100%;
 		@media ${device.laptop} {
@@ -30,47 +32,54 @@ export const Wrapper = styled.div`
 export const ContentForm = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 90%;
 	margin: 0 auto 10% auto;
+	width: 90%;
 
 	input {
 		padding-left: 20px;
 	}
 
 	h3 {
-		margin-top: 40px;
-		text-transform: uppercase;
-		text-align: center;
-		font: normal normal bold 35px/46px Montserrat;
-		letter-spacing: -0.88px;
 		color: #63c7a9;
+		letter-spacing: -0.88px;
+		font-family: 'Montserrat', sans-serif;
+		font-size: 1.8em;
+		font-weight: bold;
+		margin-top: 8%;
+		text-align: center;
+		text-transform: uppercase;
+		@media ${device.laptop} {
+			font-size: 2.1em;
+		}
 	}
 	> span {
-		text-align: left;
-		margin: 2% auto 0 auto;
-		font-size: 1em;
 		color: #363636;
+		font: 1em 'Open Sans', sans-serif;
+		margin: 2% auto 0 auto;
+		text-align: left;
+		width: 100%;
 		@media ${device.tablet} {
 			font-size: 1.1em;
 		}
 		@media ${device.laptop} {
-			width: 77%;
-			font-size: 1.2em;
+			font-size: 1.3em;
+			max-width: 745px;
+			margin: 4% auto 0 auto;
 		}
 	}
 `;
 
 export const FormGroupOne = styled.div`
-	margin-top: 5%;
-	display: flex;
 	align-items: center;
+	display: flex;
 	flex-direction: column;
+	margin-top: 5%;
 
 	@media ${device.laptop} {
 		flex-direction: row;
 		justify-content: space-between;
-		width: 70%;
-		margin: 2% auto 0 auto;
+		margin: 5% auto 0 auto;
+		width: 61%;
 	}
 
 	label {
@@ -82,38 +91,39 @@ export const FormGroupOne = styled.div`
 		}
 	}
 	input {
-		height: 48px;
 		border: 1px solid #363636;
+		height: 48px;
 		width: 100%;
-		@media ${device.laptop} {
-		}
+	}
+
+	span {
+		color: red;
 	}
 `;
 
 export const FormGroupTwo = styled.div`
-	margin-top: 3%;
-
 	display: flex;
 	flex-direction: column;
+	margin-top: 3%;
 
 	@media ${device.laptop} {
-		width: 70%;
 		margin: 3% auto 0 auto;
+		width: 61%;
 	}
-
+	span {
+		color: red;
+	}
 	input {
-		padding: 11px 0 135px 20px;
 		border: 1px solid #363636;
+		padding: 11px 0 135px 20px;
 		width: 100%;
-		@media ${device.laptop} {
-		}
 	}
 	button {
-		border: none;
-		margin-top: 5%;
-		height: 48px;
-		background: #63c7a9;
 		color: white;
+		background: #63c7a9;
+		border: none;
+		height: 48px;
+		margin-top: 5%;
 		@media ${device.laptop} {
 			width: 50%;
 		}
@@ -122,13 +132,28 @@ export const FormGroupTwo = styled.div`
 
 export const Footer = styled.div`
 	background-color: #363636;
-
-	height: 200px;
+	height: 414px;
+	position: relative;
+	text-align: center;
 	width: 100%;
+	z-index: 0;
 
-	h1 {
-		color: white;
-		text-align: right;
-		margin: 5% 0 0 0;
+	> div {
+		border-radius: 100%;
+		background: #ffffff;
+		height: 80px;
+		padding: 13px 0 0 0;
+		position: absolute;
+		top: 26%;
+		right: 8%;
+		width: 90px;
+		transition: all 0.6s ease-in-out;
+	}
+
+	a {
+		text-decoration: none;
+		color: #363636;
+		display: inline-block;
+		font-size: 4em;
 	}
 `;
