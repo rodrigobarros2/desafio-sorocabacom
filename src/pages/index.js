b0b1076050ffc675a805ee47060f8a18f455e5b6
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-12-16 13:37:05 -0300

    Add: Component Form

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 import Card from '../components/Card';
 import Carousel from '../components/Carousel';
+import Form from '../components/Form';
 
 const App = () => {
 	return (
 		<>
 			<Card />
 			<Carousel />
+			<Form />
 		</>
 	);
 };
