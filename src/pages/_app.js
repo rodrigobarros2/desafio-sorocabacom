d279a61ba4267efb3d2f50734f019993325b6e27
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-12-17 06:18:35 -0300

    Add: Page title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Head from 'next/head';
 import GlobalStyle from '../styles/global';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Head>
+				<title>Teste Sorocabacom</title>
+			</Head>
 			<GlobalStyle />
 			<Component {...pageProps} />
 		</>
